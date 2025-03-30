e54b73a568513ac02d67d65a686d9dcd5c59d08b
Luccas Caron <luccascaron1@gmail.com>
2025-03-29 21:28:15 -0300

    fix

--- solver/warmup_solver.c
+++ solver/warmup_solver.c
@@ -4,6 +4,29 @@
 #define OUTPUT_DIR "output/"         
 #define SOLUTION_FILE "solution.txt"
 
+void solve_warmup(FILE* ptr_in_file, char* file_name, const char* warmup_instance) {
+    int quantidadeDeAlunos, qtdPessoaCabine, resultado;
+
+    // Lendo os valores do arquivo de entrada
+    if (fscanf(ptr_in_file, "%d %d", &qtdPessoaCabine, &quantidadeDeAlunos) != 2) {
+        printf("Erro ao ler os valores do arquivo de entrada!\n");
+        exit(1);
+    }
+
+    // Calculando a quantidade de viagens
+    resultado = calculaViagem(quantidadeDeAlunos, qtdPessoaCabine);
+    printf("Resultado calculado: %d\n", resultado);
+
+    // Criando o arquivo de solução e escrevendo o resultado calculado
+    FILE *fwsolptr = fopen(SOLUTION_FILE, "w");
+    if (fwsolptr == NULL) {
+        printf("Erro ao abrir o arquivo de solução '%s'\n", SOLUTION_FILE);
+        exit(1);
+    }
+    fprintf(fwsolptr, "%d\n", resultado);
+    fclose(fwsolptr);
+}
+
 
 int calculaViagem(int quantidadeDeAlunos, int qtdPessoaCabine) {
     int qtdViagem = 1;
@@ -13,6 +36,7 @@ int calculaViagem(int quantidadeDeAlunos, int qtdPessoaCabine) {
     }
 
     while (quantidadeDeAlunos >= qtdPessoaCabine) {
+        quantidadeDeAlunos ++;
         quantidadeDeAlunos -= qtdPessoaCabine; 
         qtdViagem++;
     }
@@ -20,36 +44,7 @@ int calculaViagem(int quantidadeDeAlunos, int qtdPessoaCabine) {
     return qtdViagem;
 }
 
-
-void solve_warmup(FILE* ptr_in_file, char* file_name, const char* warmup_instance) {
-    FILE *fwsolptr;
-    int quantidadeDeAlunos, qtdPessoaCabine, resultado;
-
-    // Lendo os valores do arquivo de entrada
-    if (fscanf(ptr_in_file, "%d %d",  &qtdPessoaCabine, &quantidadeDeAlunos) != 2) {
-        printf("Erro ao ler os valores do arquivo de entrada!\n");
-        exit(1);
-    }
-
-    // Calculando a quantidade de viagens
-    resultado = calculaViagem(quantidadeDeAlunos, qtdPessoaCabine);
-
-    // Criando o arquivo de solução
-    fwsolptr = fopen(SOLUTION_FILE, "w");
-    if (fwsolptr == NULL) {
-        printf("Erro ao abrir o arquivo de solução '%s'\n", SOLUTION_FILE);
-        exit(1);
-    }
-
-    // Escrevendo o resultado no arquivo de solução
-    fprintf(fwsolptr, "%d\n", resultado);
-
-    fclose(fwsolptr);
-}
-
-
 int check_warmup_solution(const char* file_name, const char* warmup_instance) {
-
     FILE *fanswer, *fsolution;
     char answer_line[100], solution_line[100], answer_file[100];
     int is_correct = 1;
@@ -59,29 +54,33 @@ int check_warmup_solution(const char* file_name, const char* warmup_instance) {
     strcat(answer_file, OUTPUT_DIR);
     strcat(answer_file, file_name);
 
-    // Opening answer file
+    // Abrindo o arquivo de resposta (valor esperado)
     fanswer = fopen(answer_file, "r");
     if (fanswer == NULL) {
-        printf("File '%s' can't be opened\n", answer_file);
+        printf("Arquivo '%s' não pode ser aberto\n", answer_file);
         exit(1);
     }
 
-    // Opening solution file
+    // Abrindo o arquivo de solução (resultado calculado)
     fsolution = fopen(SOLUTION_FILE, "r");
     if (fsolution == NULL) {
-        printf("File '%s' can't be opened\n", SOLUTION_FILE);
+        printf("Arquivo '%s' não pode ser aberto\n", SOLUTION_FILE);
         exit(1);
     }
 
-    // Reading from the answer file and comparing with the solution file
-    while (fgets(answer_line, 100, fanswer)) {
+    // Lendo a primeira linha de cada arquivo
+    if (fgets(answer_line, 100, fanswer) && fgets(solution_line, 100, fsolution)) {
+        // Remover quebra de linha, se houver
+        size_t len = strlen(answer_line);
+        if (len > 0 && answer_line[len-1] == '\n') answer_line[len-1] = '\0';
+        len = strlen(solution_line);
+        if (len > 0 && solution_line[len-1] == '\n') solution_line[len-1] = '\0';
 
-        fgets(solution_line, 100, fsolution);
-
-        if (strcmp(answer_line, solution_line)) {
+        if (strcmp(answer_line, solution_line) != 0) {
             is_correct = 0;
-            break;
         }
+    } else {
+        is_correct = 0;
     }
 
     fclose(fanswer);
